42b76338bb92ad1040bab5e1473803d60c54ba2b
jiyoonchol <jiyoon276479@gmail.com>
2023-02-14 16:47:31 +0900

    기능1

---
+++ src/main/java/com/Solo/TodoApp/ToDoApp.java
@@ -0,0 +1,10 @@
+package com.Solo.TodoApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToDoApp {
+    @GetMapping("/")
+    public String getToDoApp() {return "To-do Application!";}
+}
